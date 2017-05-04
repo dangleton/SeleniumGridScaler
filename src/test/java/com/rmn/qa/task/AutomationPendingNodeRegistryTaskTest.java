78a40177eb37d7497764c8ded773dadcade7990e
denisa <denisa@qualcomm.com>
2017-05-04 15:37:17 -0700

    test fix

--- src/test/java/com/rmn/qa/task/AutomationPendingNodeRegistryTaskTest.java
+++ src/test/java/com/rmn/qa/task/AutomationPendingNodeRegistryTaskTest.java
@@ -1,5 +1,8 @@
 package com.rmn.qa.task;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 import org.openqa.grid.internal.ProxySet;
 import org.openqa.grid.internal.utils.configuration.GridNodeConfiguration;
@@ -17,100 +20,89 @@ import junit.framework.Assert;
  */
 public class AutomationPendingNodeRegistryTaskTest extends BaseTest {
 
-    @Test
-    public void testPendingNodeRemoved() {
-        MockAutomationPendingNodeRegistryTask task = new MockAutomationPendingNodeRegistryTask(null);
-        ProxySet proxySet = new ProxySet(false);
-        MockRemoteProxy proxy = new MockRemoteProxy();
-        proxySet.add(proxy);
-        GridNodeConfiguration config = new GridNodeConfiguration();
-        String instanceId = "instanceId";
-        AutomationContext.getContext().addPendingNode(instanceId);
-        config.custom.put(AutomationConstants.INSTANCE_ID, instanceId);
-        proxy.setConfig(config);
-        proxy.setCapabilityMatcher(new AutomationCapabilityMatcher());
-        task.setProxySet(proxySet);
+	@Test
+	public void testPendingNodeRemoved() {
+		MockAutomationPendingNodeRegistryTask task = new MockAutomationPendingNodeRegistryTask(null);
+		ProxySet proxySet = new ProxySet(false);
+		MockRemoteProxy proxy = new MockRemoteProxy();
+		proxySet.add(proxy);
+		GridNodeConfiguration config = new GridNodeConfiguration();
+		String instanceId = "instanceId";
+		AutomationContext.getContext().addPendingNode(instanceId);
+		config.custom.put(AutomationConstants.INSTANCE_ID, instanceId);
+		proxy.setConfig(config);
+		proxy.setCapabilityMatcher(new AutomationCapabilityMatcher());
+		task.setProxySet(proxySet);
 
-        Assert.assertTrue("Node should be pending before task runs",
-                AutomationContext.getContext().pendingNodeExists(instanceId));
-        task.doWork();
-        Assert.assertFalse("Instance should not show as pending after task has run",
-                AutomationContext.getContext().pendingNodeExists(instanceId));
-    }
+		Assert.assertTrue("Node should be pending before task runs", AutomationContext.getContext().pendingNodeExists(instanceId));
+		task.doWork();
+		Assert.assertFalse("Instance should not show as pending after task has run", AutomationContext.getContext().pendingNodeExists(instanceId));
+	}
 
-    @Test
-    public void testPendingNodeNotRemovedMismatchInstanceId() {
-        MockAutomationPendingNodeRegistryTask task = new MockAutomationPendingNodeRegistryTask(null);
-        ProxySet proxySet = new ProxySet(false);
-        MockRemoteProxy proxy = new MockRemoteProxy();
-        proxySet.add(proxy);
-        GridNodeConfiguration config = new GridNodeConfiguration();
-        String instanceId = "instanceId";
-        String instanceIdDifferent = "differentInstanceId";
-        AutomationContext.getContext().addPendingNode(instanceIdDifferent);
-        config.custom.put(AutomationConstants.INSTANCE_ID, instanceId);
-        proxy.setConfig(config);
-        proxy.setCapabilityMatcher(new AutomationCapabilityMatcher());
-        task.setProxySet(proxySet);
+	@Test
+	public void testPendingNodeNotRemovedMismatchInstanceId() {
+		MockAutomationPendingNodeRegistryTask task = new MockAutomationPendingNodeRegistryTask(null);
+		ProxySet proxySet = new ProxySet(false);
+		MockRemoteProxy proxy = new MockRemoteProxy();
+		proxySet.add(proxy);
+		GridNodeConfiguration config = new GridNodeConfiguration();
+		String instanceId = "instanceId";
+		String instanceIdDifferent = "differentInstanceId";
+		AutomationContext.getContext().addPendingNode(instanceIdDifferent);
+		config.custom.put(AutomationConstants.INSTANCE_ID, instanceId);
+		proxy.setConfig(config);
+		proxy.setCapabilityMatcher(new AutomationCapabilityMatcher());
+		task.setProxySet(proxySet);
 
-        Assert.assertTrue("Node should be pending before task runs",
-                AutomationContext.getContext().pendingNodeExists(instanceIdDifferent));
-        task.doWork();
-        Assert.assertTrue("Instance should still be pending as it hasn't come online",
-                AutomationContext.getContext().pendingNodeExists(instanceIdDifferent));
-        Assert.assertFalse("Instance should not be pending as it never was pending to begin with",
-                AutomationContext.getContext().pendingNodeExists(instanceId));
+		Assert.assertTrue("Node should be pending before task runs", AutomationContext.getContext().pendingNodeExists(instanceIdDifferent));
+		task.doWork();
+		Assert.assertTrue("Instance should still be pending as it hasn't come online", AutomationContext.getContext().pendingNodeExists(instanceIdDifferent));
+		Assert.assertFalse("Instance should not be pending as it never was pending to begin with", AutomationContext.getContext().pendingNodeExists(instanceId));
 
-    }
+	}
 
-    @Test
-    public void testPendingNodeNotRemovedNoConfigId() {
-        MockAutomationPendingNodeRegistryTask task = new MockAutomationPendingNodeRegistryTask(null);
-        ProxySet proxySet = new ProxySet(false);
-        MockRemoteProxy proxy = new MockRemoteProxy();
-        proxySet.add(proxy);
-        GridNodeConfiguration config = new GridNodeConfiguration();
-        String instanceId = "instanceId";
-        AutomationContext.getContext().addPendingNode(instanceId);
-        proxy.setConfig(config);
-        proxy.setCapabilityMatcher(new AutomationCapabilityMatcher());
-        task.setProxySet(proxySet);
+	@Test
+	public void testPendingNodeNotRemovedNoConfigId() {
+		MockAutomationPendingNodeRegistryTask task = new MockAutomationPendingNodeRegistryTask(null);
+		ProxySet proxySet = new ProxySet(false);
+		MockRemoteProxy proxy = new MockRemoteProxy();
+		proxySet.add(proxy);
+		GridNodeConfiguration config = new GridNodeConfiguration();
+		String instanceId = "instanceId";
+		AutomationContext.getContext().addPendingNode(instanceId);
+		proxy.setConfig(config);
+		proxy.setCapabilityMatcher(new AutomationCapabilityMatcher());
+		task.setProxySet(proxySet);
 
-        Assert.assertTrue("Node should be pending before task runs",
-                AutomationContext.getContext().pendingNodeExists(instanceId));
-        task.doWork();
-        Assert.assertTrue("Node should still be pending after task runs as there was no instance id in the config",
-                AutomationContext.getContext().pendingNodeExists(instanceId));
-    }
+		Assert.assertTrue("Node should be pending before task runs", AutomationContext.getContext().pendingNodeExists(instanceId));
+		task.doWork();
+		Assert.assertTrue("Node should still be pending after task runs as there was no instance id in the config", AutomationContext.getContext().pendingNodeExists(instanceId));
+	}
 
-    @Test
-    public void testPendingNodeNotRemovedNullProxySet() {
-        MockAutomationPendingNodeRegistryTask task = new MockAutomationPendingNodeRegistryTask(null);
-        ProxySet proxySet = new ProxySet(false);
-        String instanceId = "instanceId";
-        AutomationContext.getContext().addPendingNode(instanceId);
-        task.setProxySet(proxySet);
+	@Test
+	public void testPendingNodeNotRemovedNullProxySet() {
+		MockAutomationPendingNodeRegistryTask task = new MockAutomationPendingNodeRegistryTask(null);
+		ProxySet proxySet = new ProxySet(false);
+		String instanceId = "instanceId";
+		AutomationContext.getContext().addPendingNode(instanceId);
+		task.setProxySet(proxySet);
 
-        Assert.assertTrue("Node should be pending before task runs",
-                AutomationContext.getContext().pendingNodeExists(instanceId));
-        task.doWork();
-        Assert.assertTrue("Node should still be pending after task runs as there was no instance id in the config",
-                AutomationContext.getContext().pendingNodeExists(instanceId));
-    }
+		Assert.assertTrue("Node should be pending before task runs", AutomationContext.getContext().pendingNodeExists(instanceId));
+		task.doWork();
+		Assert.assertTrue("Node should still be pending after task runs as there was no instance id in the config", AutomationContext.getContext().pendingNodeExists(instanceId));
+	}
 
-    @Test
-    public void testPendingNodeNotRemovedEmptyProxySet() {
-        MockAutomationPendingNodeRegistryTask task = new MockAutomationPendingNodeRegistryTask(null);
-        ProxySet proxySet = new ProxySet(false);
-        String instanceId = "instanceId";
-        AutomationContext.getContext().addPendingNode(instanceId);
-        task.setProxySet(proxySet);
+	@Test
+	public void testPendingNodeNotRemovedEmptyProxySet() {
+		MockAutomationPendingNodeRegistryTask task = new MockAutomationPendingNodeRegistryTask(null);
+		ProxySet proxySet = new ProxySet(false);
+		String instanceId = "instanceId";
+		AutomationContext.getContext().addPendingNode(instanceId);
+		task.setProxySet(proxySet);
 
-        Assert.assertTrue("Node should be pending before task runs",
-                AutomationContext.getContext().pendingNodeExists(instanceId));
-        task.doWork();
-        Assert.assertTrue("Node should still be pending after task runs as there was no instance id in the config",
-                AutomationContext.getContext().pendingNodeExists(instanceId));
-    }
+		Assert.assertTrue("Node should be pending before task runs", AutomationContext.getContext().pendingNodeExists(instanceId));
+		task.doWork();
+		Assert.assertTrue("Node should still be pending after task runs as there was no instance id in the config", AutomationContext.getContext().pendingNodeExists(instanceId));
+	}
 
 }
